1409dfd3da14975188a82697ba5b62e95e191512
rodrigo <rodrigo.garcia@warriorslabs.com>
2018-02-08 02:11:55 -0600

    Creacion del Facade para implementar el sdk MercadoPago en laravel

---
+++ src/Facades/MP.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace rodggall\MercadoPago\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class MP extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'MP';
+    }
+}
