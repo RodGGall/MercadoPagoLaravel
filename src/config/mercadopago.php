68442b9d224dfa6da641dbbaaebc474d4c30a724
rodrigo <rodrigo.garcia@warriorslabs.com>
2018-02-08 09:16:22 -0600

    Implementacion automatica de valores para sdk desde config/mercadopago

--- src/config/mercadopago.php
+++ src/config/mercadopago.php
@@ -1,7 +1,18 @@
 <?php
 
-return [
-	'app_access_token'=> env('MP_APP_ACCESS_TOKEN',''),
-	'app_id'     => env('MP_APP_ID', ''),
-	'app_secret' => env('MP_APP_SECRET', '')
-];
+if(isset(env('MP_APP_ACCESS_TOKEN'))){
+	return[
+		'app_access_token'=> env('MP_APP_ACCESS_TOKEN')
+	];
+}
+elseif(isset(env('MP_APP_ID')) && isset(env('MP_APP_SECRET'))){
+	return [
+		'app_id'     => env('MP_APP_ID'),
+		'app_secret' => env('MP_APP_SECRET')
+	];
+}
+else{
+	'app_access_token'=> env('MP_APP_ACCESS_TOKEN'),
+	'app_id'     => env('MP_APP_ID'),
+	'app_secret' => env('MP_APP_SECRET')
+}
