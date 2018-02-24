fccc2f16a68a1b87128044789567e85f8faf24df
Manuel <manuel.solis@warriorslabs.com>
2018-02-24 00:19:28 -0600

    Correccion de errores en config

--- src/config/mercadopago.php
+++ src/config/mercadopago.php
@@ -1,18 +1,23 @@
 <?php
 
-if(isset(env('MP_APP_ACCESS_TOKEN'))){
+$acces_token=env('MP_APP_ACCESS_TOKEN');
+$secret=env('MP_APP_SECRET');
+$id=env('MP_APP_ID');
+if(isset($acces_token)){
 	return[
 		'app_access_token'=> env('MP_APP_ACCESS_TOKEN')
 	];
 }
-elseif(isset(env('MP_APP_ID')) && isset(env('MP_APP_SECRET'))){
+elseif(isset($id) && isset($secret)){
 	return [
 		'app_id'     => env('MP_APP_ID'),
 		'app_secret' => env('MP_APP_SECRET')
 	];
 }
 else{
-	'app_access_token'=> env('MP_APP_ACCESS_TOKEN'),
-	'app_id'     => env('MP_APP_ID'),
-	'app_secret' => env('MP_APP_SECRET')
+    return [
+	   'app_access_token' => env('MP_APP_ACCESS_TOKEN'),
+	   'app_id'     => env('MP_APP_ID'),
+	   'app_secret' => env('MP_APP_SECRET')
+    ];
 }
