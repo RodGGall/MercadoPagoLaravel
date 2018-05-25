4c782b2c9d87c151ff49a15a6854e8105730424d
RodGGall <rodggall@gmail.com>
2018-05-25 10:29:45 -0500

    Llave sin cerrar

--- src/MP.php
+++ src/MP.php
@@ -290,3 +290,4 @@ class MercadoPagoException extends Exception {
     public function __construct($message, $code = 500, Exception $previous = null) {
         parent::__construct($message, $code, $previous);
     }
+}
